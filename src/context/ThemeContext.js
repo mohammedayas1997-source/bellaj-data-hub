3615363e94ad0edb9f479c0d03b0f5d1e9e87332
Abdulrahman <mohammedayas1997@gmail.com>
2026-05-22 05:29:50 -0700

    fix: correct import path and named import for ThemeProvider

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -4,7 +4,6 @@ export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   return (
     <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
       {children}
