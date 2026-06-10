9e23690fdeba9a8cfc6575ffbc0723c7a0db78dc
Abdulrahman Mohammed <mohammedayas1997@gmail.com>
2026-06-10 06:49:44 -0700

    Update frontend API base URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,4 @@
-const BASE_URL = "https://bellaj-data-server.onrender.com/api/v1";
+const BASE_URL =
+  process.env.API_BASE_URL || "https://bellaj-data-server.onrender.com/api/v1";
 
 export default BASE_URL;
